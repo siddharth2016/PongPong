2950bfb259b5b0fda8356c4d58a79ef5cd9cb695
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-11 21:44:22 +0530

    using load and update methods

--- pongpong.py
+++ pongpong.py
@@ -1,5 +1,5 @@
 import pyglet
-from pong import rectangle, ball, paddle
+from pong import rectangle, ball, paddle, load
 
 #Variables
 WIDTH = 600
@@ -9,29 +9,32 @@ RADIUS = 12
 PWIDTH = 120
 PHEIGHT = 15
 
+
 class PongPongWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super(PongPongWindow, self).__init__(*args, **kwargs)
 
+        self.win_size = (WIDTH, HEIGHT)
+        self.paddle_pos = (WIDTH/2-PWIDTH/2, 0)
         self.main_batch = pyglet.graphics.Batch()
-        self.top_rect = rectangle.RectangleObject(x=0, y=HEIGHT-BORDER, width=WIDTH, height=BORDER, batch=self.main_batch)
-        self.left_rect = rectangle.RectangleObject(x=0, y=0, width=BORDER, height=HEIGHT, batch=self.main_batch)
-        self.right_rect = rectangle.RectangleObject(x=WIDTH-BORDER, y=0, width=BORDER, height=HEIGHT, batch=self.main_batch)
-        self.ball = ball.BallObject(x=WIDTH/2, y=HEIGHT/2, radius=RADIUS, batch=self.main_batch)
-        self.paddle = paddle.Paddle(x=WIDTH/2-PWIDTH/2, y=0, width=PWIDTH, height=PHEIGHT, batch=self.main_batch)
+        self.walls = load.load_rectangles(self.win_size, BORDER, batch=self.main_batch)
+        self.balls = load.load_balls(self.win_size, RADIUS, num_balls=1, paddle_pos=self.paddle_pos, batch=self.main_batch)
+        self.paddles = load.load_paddles(self.paddle_pos, PWIDTH, PHEIGHT, batch=self.main_batch)
 
     def on_draw(self):
         self.clear()
         self.main_batch.draw()
 
+
 game_window = PongPongWindow(width=WIDTH, height=HEIGHT, caption='PongPong')
-game_objects = [game_window.ball, game_window.paddle]
+game_objects = game_window.balls + game_window.paddles
+
 
 def update(dt):
     global game_objects, game_window
 
     for obj in game_objects:
-        obj.update(dt)
+        obj.update(game_window.win_size, BORDER, dt)
 
 
 if __name__ == '__main__':
