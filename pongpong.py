790ee9ee66a6e90bab832b4118e1bea26cc043d2
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-11 16:43:46 +0530

    PongPong all objects in place

--- pongpong.py
+++ pongpong.py
@@ -1,11 +1,13 @@
 import pyglet
-from pong import rectangle, ball
+from pong import rectangle, ball, paddle
 
 #Variables
 WIDTH = 600
 HEIGHT = 600
 BORDER = 10
-
+RADIUS = 12
+PWIDTH = 120
+PHEIGHT = 15
 
 class PongPongWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
@@ -15,7 +17,8 @@ class PongPongWindow(pyglet.window.Window):
         self.top_rect = rectangle.RectangleObject(x=0, y=HEIGHT-BORDER, width=WIDTH, height=BORDER, batch=self.main_batch)
         self.left_rect = rectangle.RectangleObject(x=0, y=0, width=BORDER, height=HEIGHT, batch=self.main_batch)
         self.right_rect = rectangle.RectangleObject(x=WIDTH-BORDER, y=0, width=BORDER, height=HEIGHT, batch=self.main_batch)
-
+        self.ball = ball.BallObject(x=WIDTH/2, y=HEIGHT/2, radius=RADIUS, batch=self.main_batch)
+        self.paddle = paddle.Paddle(x=WIDTH/2-PWIDTH/2, y=0, width=PWIDTH, height=PHEIGHT, batch=self.main_batch)
 
     def on_draw(self):
         self.clear()
