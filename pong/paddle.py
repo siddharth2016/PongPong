7cd50758af488ee9b3aa24e56e276a44be3cea60
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-11 22:11:33 +0530

    remove collide with

--- pong/paddle.py
+++ pong/paddle.py
@@ -6,8 +6,5 @@ class Paddle(pyglet.shapes.Rectangle):
     def __init__(self, *args, **kwargs):
         super(Paddle, self).__init__(*args, **kwargs)
 
-    def update(self, win_size, border, dt):
-        pass
-
-    def collide_with(self, win_size, other_object, dt):
+    def update(self, win_size, border):
         pass
