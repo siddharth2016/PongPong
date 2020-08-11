4035ce10f38ea725fccd07d187e26ddf0936f50c
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-11 22:10:00 +0530

    ball collide_with walls complete

--- pong/ball.py
+++ pong/ball.py
@@ -1,5 +1,4 @@
 import pyglet
-import random
 
 
 class BallObject(pyglet.shapes.Circle):
@@ -9,13 +8,13 @@ class BallObject(pyglet.shapes.Circle):
         self.color = (255, 180, 0)
         self.velocity_x, self.velocity_y = 0.0, 0.0
 
-    def update(self, win_size, border, dt):
-        newx = self.x + self.velocity_x * dt
-        newy = self.y + self.velocity_y * dt
+    def update(self, win_size, border):
+        newx = self.x + self.velocity_x
+        newy = self.y + self.velocity_y
 
         if newx < border + self.radius or newx > win_size[0] - border - self.radius:
             self.velocity_x = -self.velocity_x
-        elif newy < win_size[1] - border - self.radius:
+        elif newy > win_size[1] - border - self.radius:
             self.velocity_y = -self.velocity_y
         else:
             self.x = newx
