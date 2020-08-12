aac3781a7165a52d20b38a4bb114d7e949943d6c
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-12 17:45:31 +0530

    PongPongWindow optimization, event handlers and update method refactoring

--- pongpong.py
+++ pongpong.py
@@ -1,13 +1,15 @@
 import pyglet
 from pong import rectangle, ball, paddle, load
 
-#Variables, Considering a vertical oriented window for game
+# Variables, Considering a vertical oriented window for game
 WIDTH = 600
 HEIGHT = 600
 BORDER = 10
 RADIUS = 12
-PWIDTH = 120 #Paddle Width
-PHEIGHT = 15 #Paddle Height
+PWIDTH = 120  # Paddle Width
+PHEIGHT = 15  # Paddle Height
+ballspeed = (-2, -2)    # Initially ball will be falling with speed (x, y)
+paddleacc = (-5, 5)   # Paddle Acceleration on both sides - left: negative acc, right: positive acc, for x-axis
 
 
 class PongPongWindow(pyglet.window.Window):
@@ -18,8 +20,8 @@ class PongPongWindow(pyglet.window.Window):
         self.paddle_pos = (WIDTH/2-PWIDTH/2, 0)
         self.main_batch = pyglet.graphics.Batch()
         self.walls = load.load_rectangles(self.win_size, BORDER, batch=self.main_batch)
-        self.balls = load.load_balls(self.win_size, RADIUS, batch=self.main_batch)
-        self.paddles = load.load_paddles(self.paddle_pos, PWIDTH, PHEIGHT, batch=self.main_batch)
+        self.balls = load.load_balls(self.win_size, RADIUS, speed=ballspeed, batch=self.main_batch)
+        self.paddles = load.load_paddles(self.paddle_pos, PWIDTH, PHEIGHT, acc=paddleacc, batch=self.main_batch)
 
     def on_draw(self):
         self.clear()
@@ -29,6 +31,10 @@ class PongPongWindow(pyglet.window.Window):
 game_window = PongPongWindow(width=WIDTH, height=HEIGHT, caption='PongPong')
 game_objects = game_window.balls + game_window.paddles
 
+for paddle in game_window.paddles:
+    for handler in paddle.event_handlers:
+        game_window.push_handlers(handler)
+
 
 def update(dt):
     global game_objects, game_window
@@ -37,7 +43,7 @@ def update(dt):
         for obj2 in game_objects:
             if obj1 is obj2:
                 continue
-            obj1.update(game_window.win_size, BORDER, obj2)
+            obj1.update(game_window.win_size, BORDER, obj2, dt)
 
 
 if __name__ == '__main__':
