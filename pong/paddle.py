841639dd93bc19aa0d626471e926aee544294470
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-12 17:43:36 +0530

    paddle update method and key_handler

--- pong/paddle.py
+++ pong/paddle.py
@@ -1,4 +1,5 @@
 import pyglet
+from pyglet.window import key
 
 
 class Paddle(pyglet.shapes.Rectangle):
@@ -6,5 +7,26 @@ class Paddle(pyglet.shapes.Rectangle):
     def __init__(self, *args, **kwargs):
         super(Paddle, self).__init__(*args, **kwargs)
 
-    def update(self, win_size, border):
-        pass
+        self.acc_left, self.acc_right = 0.0, 0.0
+        self.rightx = 0
+        self.key_handler = key.KeyStateHandler()
+        self.event_handlers = [self, self.key_handler]
+
+    def update(self, win_size, border, other_object, dt):
+
+        newlx = self.x + self.acc_left
+        newrx = self.x + self.acc_right
+
+        if self.key_handler[key.LEFT]:
+            self.x = newlx
+        elif self.key_handler[key.RIGHT]:
+            self.x = newrx
+
+        self.rightx = self.x + self.width
+
+        if self.x < border:
+            self.x = border
+            self.rightx = self.x + self.width
+        elif self.rightx > win_size[0]-border:
+            self.x = win_size[0]-border-self.width
+            self.rightx = self.x + self.width
