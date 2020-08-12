6e5058e3c868d79b90b8a5ec8abe717e7bdb9775
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-12 14:18:30 +0530

    PongPong with complete ball function

--- pongpong.py
+++ pongpong.py
@@ -1,13 +1,13 @@
 import pyglet
 from pong import rectangle, ball, paddle, load
 
-#Variables
+#Variables, Considering a vertical oriented window for game
 WIDTH = 600
 HEIGHT = 600
 BORDER = 10
 RADIUS = 12
-PWIDTH = 120
-PHEIGHT = 15
+PWIDTH = 120 #Paddle Width
+PHEIGHT = 15 #Paddle Height
 
 
 class PongPongWindow(pyglet.window.Window):
@@ -18,7 +18,7 @@ class PongPongWindow(pyglet.window.Window):
         self.paddle_pos = (WIDTH/2-PWIDTH/2, 0)
         self.main_batch = pyglet.graphics.Batch()
         self.walls = load.load_rectangles(self.win_size, BORDER, batch=self.main_batch)
-        self.balls = load.load_balls(self.win_size, RADIUS, num_balls=1, paddle_pos=self.paddle_pos, batch=self.main_batch)
+        self.balls = load.load_balls(self.win_size, RADIUS, batch=self.main_batch)
         self.paddles = load.load_paddles(self.paddle_pos, PWIDTH, PHEIGHT, batch=self.main_batch)
 
     def on_draw(self):
@@ -33,8 +33,11 @@ game_objects = game_window.balls + game_window.paddles
 def update(dt):
     global game_objects, game_window
 
-    for obj in game_objects:
-        obj.update(game_window.win_size, BORDER)
+    for obj1 in game_objects:
+        for obj2 in game_objects:
+            if obj1 is obj2:
+                continue
+            obj1.update(game_window.win_size, BORDER, obj2)
 
 
 if __name__ == '__main__':
