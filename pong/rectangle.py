3d694cd2aeabd3afa844b9493d529f895a6e862f
siddharth2016 <siddharthchandragzb@gmail.com>
2021-01-28 19:03:34 +0530

    change rectangle.py for part1

--- pong/rectangle.py
+++ pong/rectangle.py
@@ -1,8 +1,8 @@
-import pyglet
+# ./PongPong/pong/rectangle.py
 
+import pyglet
 
 class RectangleObject(pyglet.shapes.Rectangle):
 
     def __init__(self, *args, **kwargs):
         super(RectangleObject, self).__init__(*args, **kwargs)
-
