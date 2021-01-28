54998c7d55e644ff78e8b69d5e5f28462c3461a7
siddharth2016 <siddharthchandragzb@gmail.com>
2021-01-28 19:10:36 +0530

    change paddle.py for part3

--- pong/paddle.py
+++ pong/paddle.py
@@ -14,3 +14,23 @@ class Paddle(pyglet.shapes.Rectangle):
         self.rightx = 0
         self.key_handler = key.KeyStateHandler()
         self.event_handlers = [self, self.key_handler]
+
+
+    def update(self, win_size : Tuple, border : float, other_object, dt):
+
+        newlx = self.x + self.acc_left
+        newrx = self.x + self.acc_right
+
+        if self.key_handler[key.LEFT]:
+            self.x = newlx
+        elif self.key_handler[key.RIGHT]:
+            self.x = newrx
+
+        self.rightx = self.x + self.width
+
+        if self.x < border:
+            self.x = border
+            self.rightx = self.x + self.width
+        elif self.rightx > win_size[0]-border:
+            self.x = win_size[0]-border-self.width
+            self.rightx = self.x + self.width
